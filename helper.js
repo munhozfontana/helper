07eab5f969ac667a72c8d3f6cbca9ef0f3ba080d
luis neto <munhozfontana@gmail.com>
2023-11-01 20:45:10 -0300

    typ

--- helper.js
+++ helper.js
@@ -1,94 +1,116 @@
-function noBreakSpace(body, ignoreAttributes) {
+function noBreakSpace(body: string, ignoreAttributes: string[]): string {
   try {
     const jsonObject = JSON.parse(body);
-    function processObject(obj, olderKey) {
 
-      for (const key in obj) {
-        if (ignoreAttributes.includes(currentPath(olderKey, key).join('.').replace(/\.\d/g, ''))) {
-          continue;
-        }
-        if (typeof obj[key] === 'string') {
-          replcaeRule(obj, key);
-        } else if (Array.isArray(obj[key])) {
-          whenIsArray(obj, key, processObject, olderKey);
-        } else {
-          processObject(obj[key], [...(olderKey ?? []), key]);
+    function processObject(obj: any, path: (string | number)[] = []): any {
+      if (typeof obj === 'string') {
+        obj = obj.replaceAll('@', ' ');
+      } else if (Array.isArray(obj)) {
+        obj.forEach((element, index) => {
+          if (typeof element === 'object') {
+            processObject(element, [...path, index]);
+          } else {
+            obj[index] = element.replaceAll('@', ' ');
+          }
+        });
+      } else if (typeof obj === 'object' && obj !== null) {
+        for (const key in obj) {
+          const currentPath = [...path, key];
+          const currentPathString = currentPath.join('.');
+          if (ignoreAttributes.includes(currentPathString.replace(/\.\d/g, ''))) {
+            continue;
+          }
+          obj[key] = processObject(obj[key], currentPath);
         }
       }
-
+      return obj;
     }
 
-    processObject(jsonObject);
-
-    return JSON.stringify(jsonObject);
+    const processedObject = processObject(jsonObject);
+    return JSON.stringify(processedObject, null, 2);
   } catch (error) {
     console.error('Erro ao processar o JSON:', error);
     return body;
   }
 }
 
-
-
-function currentPath(olderKey, key) {
-  return [...(olderKey ?? []), key];
-}
-
-function replcaeRule(obj, key) {
-  obj[key] = obj[key].replaceAll('@', ' ');
-}
-
-function whenIsArray(obj, key, processObject, olderKey) {
-  obj[key].forEach((element, index) => {
-    if (typeof element == 'object') {
-      processObject(obj[key], [...(olderKey ?? []), key]);
-    } else {
-      obj[key][index] = element.replaceAll('@', ' ');
+const body: string = `{
+  "nome": "@",
+  "idade": 30,
+  "endereco": {
+    "rua": "@@",
+    "cidade": "@@",
+    "estado": "@@"
+  },
+  "emails": [
+    "@@@",
+    "@@@"
+  ],
+  "telefones": {
+    "casa": "@@@",
+    "trabalho": "@@@",
+    "celular": "@@@",
+    "others": {
+      "email": "@@@@"
     }
-  });
-}
-
-
-
-
-const body = `{
-    "nome": "@",
-    "idade": 30,
-    "endereco": {
-      "rua": "@@",
-      "cidade": "@@",
-      "estado": "@@"
+  },
+  "amigos": [
+    {
+      "nome": "@@@@",
+      "bbb": "@@@@",
+      "idade": 28
     },
-    "emails": [
-      "@@@",
-      "@@@"
-    ],
-    "telefones": {
-      "casa": "@@@",
-      "trabalho": "@@@",
-      "celular": "@@@",
-      "others": {
-        "email": "@@@@"
-      }
-    },
-    "amigos": [
-      {
-        "nome": "@@@@",
-        "bbb": "@@@@",
-        "idade": 28
-      },
-      {
-        "nome": "@@@@",
-        "bbb": "@@@@",
-        "idade": 32
-      }
-    ]
-  }`;
+    {
+      "nome": "@@@@",
+      "bbb": "@@@@",
+      "idade": 32
+    }
+  ]
+}`;
 
+const ignoreAttributes: string[] = ['nome', 'emails', 'endereco.rua', 'amigos.nome'];
+// const ignoreAttributes: string[] = [];
 
-const ignoreAttributes = ['nome', 'emails', 'endereco.rua', 'amigos.nome',];
-// const ignoreAttributes = [];
-
-const resultado = noBreakSpace(body, ignoreAttributes);
+const resultado: string = noBreakSpace(body, ignoreAttributes);
 
 console.log(resultado);
 
+const body = `{
+  "nome": "@",
+  "idade": 30,
+  "endereco": {
+    "rua": "@@",
+    "cidade": "@@",
+    "estado": "@@"
+  },
+  "emails": [
+    "@@@",
+    "@@@"
+  ],
+  "telefones": {
+    "casa": "@@@",
+    "trabalho": "@@@",
+    "celular": "@@@",
+    "others": {
+      "email": "@@@@"
+    }
+  },
+  "amigos": [
+    {
+      "nome": "@@@@",
+      "bbb": "@@@@",
+      "idade": 28
+    },
+    {
+      "nome": "@@@@",
+      "bbb": "@@@@",
+      "idade": 32
+    }
+  ]
+}`;
+
+// const ignoreAttributes = ['nome', 'emails', 'endereco.rua', 'amigos.nome'];
+
+
+console.log(noBreakSpace(body, []));
+console.log(noBreakSpace(body, ['nome', 'emails', 'endereco.rua', 'amigos.nome']));
